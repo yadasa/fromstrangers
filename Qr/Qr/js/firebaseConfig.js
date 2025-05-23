dfbc631c9936fab2d2c8af4480c0cbb4edf83d26
yadasa <75089908+yadasa@users.noreply.github.com>
2025-05-23 03:37:53 -0500

    Add files via upload

---
+++ Qr/Qr/js/firebaseConfig.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+    apiKey:           "AIzaSyAYhXnfm9e3y5gQVUs7Viu_JkLBvE3NfzI",
+    authDomain:       "fromstrangerssocial.firebaseapp.com",
+    projectId:        "fromstrangerssocial",
+    storageBucket:    "fromstrangerssocial.firebasestorage.app",
+    messagingSenderId: "233615930576",
+    appId:            "1:233615930576:web:873500efe161e8b5649ad5"
+  };
+  
